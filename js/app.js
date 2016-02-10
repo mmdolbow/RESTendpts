557b3a837d488d31a89d27c0b2dc7ed0e2257302
mmdolbow <mmdolbow@users.noreply.github.com>
2016-02-09 21:19:12 -0600

    Building out queries text
    Also began the structure of DOM elements for demonstrating random
    queries.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**
+ * @author Mike Dolbow
+ * An application support script for demonstrating capabilities of Esri REST Endpoints.
+ */
+
+/**
+ * On Click function for btnQueryRandDemo.
+ * Picks a letter of the alphabet at random, and fires a query against the Detailed Counties
+ * layer in order to get many results if possible. (Might need to exclude "X", but "Z" works)
+ * Need to define the DOM elements to be filled in on click. Also need to define ajax queries
+ * with JQuery.
+ */
