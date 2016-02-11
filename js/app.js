0f45781d3ad7b750ca8b47cbbe8d7c0b99ced692
mmdolbow <mmdolbow@users.noreply.github.com>
2016-02-10 21:14:08 -0600

    Finished random query generator
    Includes javascript, DOM elements, etc

--- js/app.js
+++ js/app.js
@@ -10,3 +10,27 @@
  * Need to define the DOM elements to be filled in on click. Also need to define ajax queries
  * with JQuery.
  */
+
+function getRandomCounty() {
+	//pick a random letter to start a query with, excluding X
+	var chars = 'ABCDEFGHIJKLMNOPQRSTUVWYZ';
+	var randomChar = chars[Math.round(Math.random() * (chars.length - 1))];
+
+	var url1 = "https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/4/query?outFields=NAME%2C+POP2007&returnGeometry=false&f=json&text="+randomChar;
+	
+	
+	//Make an AJAX request against the Esri service to acquire counties that begin with the random letter
+	//Request 1: get the total package (resource) count
+	    $.ajax({
+        url: url1,
+        dataType: "jsonp",
+        cache: "false",
+    })
+	  .done(function( data ) {
+		$("#emQueryRandURL").html('<a target=\"_blank\" href=\"'+url1+'\">Detailed Counties Starting with '+randomChar+'<\/a>');
+		$("#emQueryNoCounties").html(data.features.length);
+		$("#emQueryFirstCounty").html(data.features[0].attributes.NAME);
+		$("#emQueryPop").html(data.features[0].attributes.POP2007)
+	  });
+
+}
