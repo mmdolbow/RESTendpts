0edb44dabc4edf2d3e331a2dafa957c566b29a02
mmdolbow <mmdolbow@users.noreply.github.com>
2016-02-16 21:02:03 -0600

    Updates, mostly for pnp demo
    Broad updates, noting (new window) where target is blank. Also completed
    first draft of pnpdemo, including functionality in app.js to include a
    query fired off a service.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,25 @@
  */
 
 /**
+ * Globals
+ * 
+*/
+
+var map;
+var mapOptions = {
+		maxZoom:7,
+		minZoom:7,
+		zoomControl:'false',
+		center: [37.0,-90.0],
+		zoom:7,
+		maxBounds:[
+			[34.5,-98.0],
+			[41.0,-79.0]
+			]
+	};
+var dynMapServiceURL = '//server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_by_Sex/MapServer';
+
+/**
  * On Click function for btnQueryRandDemo.
  * Picks a letter of the alphabet at random, and fires a query against the Detailed Counties
  * layer in order to get many results, and display some details from the first result. (Might need to exclude "X", but "Z" works)
@@ -31,15 +50,9 @@ function getRandomCounty() {
 }
 
 function initpnpmap() {	
-		var map = L.map('map', {
-				maxZoom:8,
-				minZoom:8,
-				zoomControl:'false',
-				center: [40.0,-80.0],
-				zoom:8
-			});
+	var map = L.map('map', mapOptions);
 		map.removeControl(map.zoomControl);
-		var marker = L.marker([40.0,-80.0]).addTo(map);
+		var marker = L.marker(map.getCenter()).addTo(map);
 
 		var MapQuestOpen_OSM = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg', {
 			attribution: 'Tiles Courtesy of <a href="http://polymaps.org">PolyMaps<a>, <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
@@ -48,7 +61,33 @@ function initpnpmap() {
 		map.addLayer(MapQuestOpen_OSM);
 		
 		L.esri.dynamicMapLayer({
-		    url: '//server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_by_Sex/MapServer',
+		    url: dynMapServiceURL,
 		    opacity: 0.5
 		  }).addTo(map);
+		
+		map.whenReady(function() {
+		    map.on('moveend', function() {
+		    	   	marker.setLatLng(map.getCenter());
+		    	   	mapquery(map.getCenter());
+		    	});
+		});
+		
 	}
+
+function mapquery(pntLatLng) {
+	console.log("Map was moved to: "+pntLatLng.lat+", "+pntLatLng.lng);
+	var queryURL = dynMapServiceURL+'/3/query?geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=false&f=json&geometry='+pntLatLng.lng+'%2C'+pntLatLng.lat;
+	$("#emPnPURL").html('<a target=\"_blank\" href=\"'+queryURL+'\">Query URL (new window)<\/a>');
+	//Make an AJAX request against the Esri service to acquire demographics info at the lat long
+	//sample query: https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_by_Sex/MapServer/3/query?geometry=-96.21826%2C47.88688&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=false&f=pjson
+    $.ajax({
+	    url: queryURL,
+	    dataType: "jsonp",
+	    cache: "false"
+	})
+ 	.done(function( data, url ) {
+ 		//console.table(data);
+		$("#emPnPCounty").html(data.features[0].attributes.NAME+", "+data.features[0].attributes.ST_ABBREV);
+		$("#emPnPPop").html(data.features[0].attributes.TOTPOP_CY);
+	});
+}
